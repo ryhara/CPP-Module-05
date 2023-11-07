4bccd75b9a868b51226168de18ccd5e48a285b04
ryhara <ryosei.hara@gmail.com>
2023-11-07 21:13:54 +0900

    [Add] ex00

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,78 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+	std::cout << "Bureaucrat default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int garde) : _name(name), _grade(garde)
+{
+	std::cout << "Bureaucrat constructor called" << std::endl;
+	if (this->_grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (this->_grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat destructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy)
+{
+	std::cout << "Bureaucrat copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &copy)
+{
+	std::cout << "Bureaucrat assignment operator called" << std::endl;
+	if (this != &copy)
+	{
+		const_cast<std::string &>(this->_name) = copy.getName();
+		this->_grade = copy.getGrade();
+	}
+	return (*this);
+}
+
+
+std::string const Bureaucrat::getName() const
+{
+	return (this->_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (this->_grade);
+}
+
+void Bureaucrat::incrementGrade()
+{
+	if (this->_grade - 1 < 1)
+		throw Bureaucrat::GradeTooHighException();
+	this->_grade--;
+}
+
+void Bureaucrat::decrementGrade()
+{
+	if (this->_grade + 1 > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->_grade++;
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
+}
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &burocrat)
+{
+	out << burocrat.getName() << ", bureaucrat grade " << burocrat.getGrade();
+	return (out);
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,68 @@
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	{
+		try {
+			std::cout << BOLD << "Creating a Bureaucrat with default" << END << std::endl;
+			Bureaucrat bureaucrat;
+			std::cout << bureaucrat << std::endl;
+			Bureaucrat bureaucrat2(bureaucrat);
+			std::cout << bureaucrat2 << std::endl;
+			Bureaucrat bureaucrat3;
+			bureaucrat3 = bureaucrat2;
+			std::cout << bureaucrat3 << std::endl;
+		} catch (std::exception &e) {
+			std::cout  << RED << e.what() << END << std::endl;
+		}
+	}
+	std::cout << "-------------------------------------------"	<< std::endl << std::endl;
+	{
+		try {
+			std::cout << BOLD << "Creating a Bureaucrat with grade 1" << END << std::endl;
+			Bureaucrat bureaucrat("Bureaucrat1", 1);
+			std::cout << bureaucrat << std::endl;
+			for (int i = 1; i < 200; i++)
+			{
+				bureaucrat.decrementGrade();
+				std::cout << GREEN << "Count: " << i << " Decrementing grade" << END << std::endl;
+			}
+		} catch (std::exception &e) {
+			std::cout  << RED << e.what() << END << std::endl;
+		}
+	}
+	std::cout << "-------------------------------------------"	<< std::endl << std::endl;
+	{
+		try {
+			std::cout << BOLD << "Creating a Bureaucrat with grade 150" << END << std::endl;
+			Bureaucrat bureaucrat("Bureaucrat150", 150);
+			std::cout << bureaucrat << std::endl;
+			for (int i = 1; i < 200; i++)
+			{
+				bureaucrat.incrementGrade();
+				std::cout << GREEN << "Count: " << i << " Incrementing grade" << END << std::endl;
+			}
+		} catch (std::exception &e) {
+			std::cout  << RED << e.what() << END << std::endl;
+		}
+	}
+	std::cout << "-------------------------------------------"	<< std::endl << std::endl;
+	{
+		try {
+			std::cout << BOLD << "Creating a Bureaucrat with grade 0" << END << std::endl;
+			Bureaucrat bureaucrat("Bureaucrat0", 0);
+		} catch (std::exception &e) {
+			std::cout  << RED << e.what() << END << std::endl;
+		}
+	}
+	std::cout << "-------------------------------------------"	<< std::endl << std::endl;
+	{
+		try {
+			std::cout << BOLD << "Creating a Bureaucrat with grade 151" << END << std::endl;
+			Bureaucrat bureaucrat("Bureaucrat151", 151);
+		} catch (std::exception &e) {
+			std::cout  << RED << e.what() << END << std::endl;
+		}
+	}
+	std::cout << "-------------------------------------------"	<< std::endl << std::endl;
+}
